958a6829c6aaac1d3018c3faa0b0e2e427afb598
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-14 16:13:28 -0300

    feat: create and implement ExplanadionCard and IntroductionCard

---
+++ lexpro-conect/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-screen max-h-52 bg-blue-950 text-white">
+      <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
