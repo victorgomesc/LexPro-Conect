25ba06cdbb12405381684c189ef233997bed47a6
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-15 21:13:40 -0300

    feat: create component SignupForm

--- lexpro-conect/src/components/GridNews.tsx
+++ lexpro-conect/src/components/GridNews.tsx
@@ -2,20 +2,20 @@
 
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 type Card = {
   id: number;
   title: string;
-  color: string;
   imgUrl: string;
 };
 
 const GridNews: React.FC = () => {
   const [cards, setCards] = useState<Card[]>([
-    { id: 1, title: "Ultima noticia", color: "bg-red-400", imgUrl: "fff" },
-    { id: 2, title: "A LexPro Conect lidera o merdado de software para judicial", color: "bg-blue-400", imgUrl: "ergeg" },
-    { id: 3, title: "Card 3", color: "bg-green-400", imgUrl: "dwjjhfjwh" },
-    { id: 4, title: "Card 4", color: "bg-yellow-400", imgUrl: "jdhukjd" },
+    { id: 1, title: "Ultima noticia", imgUrl: "/assets/capa.webp" },
+    { id: 2, title: "A LexPro Conect lidera o merdado de software para judicial", imgUrl: "/assets/capa.webp" },
+    { id: 3, title: "Card 3", imgUrl: "/assets/capa.webp" },
+    { id: 4, title: "Card 4", imgUrl: "/assets/capa.webp" },
   ]);
 
   // Função para reordenar os cards periodicamente
@@ -33,18 +33,19 @@ const GridNews: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 w-full gap-12 p-10">
         {cards.map((card) => (
           <motion.div
             key={card.id}
-            className={`p-10 h-[60vh] w-full rounded-xl shadow-lg text-white font-bold flex items-center justify-center ${card.color}`}
+            className={`h-[60vh] w-full rounded-xl shadow-lg text-white font-bold flex flex-col items-center justify-start`}
             layout
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ duration: 1 }}
           >
-            <h1 className="text-5xl font-bold">{card.title}</h1>
+            <Image src={card.imgUrl} alt="" width={300} height={300} quality={100} className="object-cover w-full" />
+            <h1 className="text-5xl font-bold text-black">{card.title}</h1>
           </motion.div>
         ))}
       </div>
