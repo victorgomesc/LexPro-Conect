0663a9bcedf50658e81c83aecf3361bac4370e53
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-17 08:54:04 -0300

    Salvando alterações antes de mudar de branch

--- Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801e044e4448aa17ca44e68c7e91a2e2b3e03a5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66c6698f7f750584a5697bd6a2109c8cee9addd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
